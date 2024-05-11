74113d496b480880516fbdd46ddfd8f2d985d56d
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-05-10 23:27:25 -0300

    Fazendo a tela de gameover

--- Flappy Plane/Assets/Scripts/PlayerController.cs
+++ Flappy Plane/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     float speed = 6;
     Rigidbody2D myRB;
+    [SerializeField] GameObject gameOver;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,16 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRB.velocity = Vector2.up * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            gameOver.SetActive(true);
+
+            this.enabled = false;
         }
     }
 }
